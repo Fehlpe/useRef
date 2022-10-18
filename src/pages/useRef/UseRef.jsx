59cbf3298cc2c510f50251001c3691e6b96d94f6
Fehlpe <felpearcanjo@gmail.com>
2022-10-18 19:41:34 -0300

    att final

--- src/pages/useRef/UseRef.jsx
+++ src/pages/useRef/UseRef.jsx
@@ -5,6 +5,10 @@ import { Link } from "react-router-dom";
 export default function UseRef() {
     const counterRef = useRef(0);
 
+    const buttonRef = useRef();
+
+    const inputRef = useRef();
+
     function increaseCounter() {
         counterRef.current += 1;
     }
@@ -21,6 +25,20 @@ export default function UseRef() {
             <button onClick={showCounter}>Quantas vezes o botão foi clicado</button>
             &nbsp;
             <Link to="/">Voltar</Link>
+            <br /> <br /> 
+            
+            <button onClick={() => {
+                console.log(buttonRef.current);
+                buttonRef.current.click();
+            }}>Uso de Referência</button>
+            <button ref={buttonRef} onClick={() => alert("Clicou")}>Alerta</button>
+
+            <br /> <br />
+
+            <button onClick={() => {
+                inputRef.current.value += "aaa";
+            }}>Ref em Input</button>
+            <input type="text" ref={inputRef}/>
         </>
     )
 }
