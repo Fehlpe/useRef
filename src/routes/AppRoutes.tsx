75bd7a495a389dc129daa4654e9fa975ae91a074
Andreloren <andre.lorente83@gmail.com>
2022-10-18 19:57:33 -0300

    erro de sintaxe

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,17 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/home/Home";
+import Home from "../pages/Home/Home";
 import UseRef from "../pages/useRef/UseRef";
 import UseState from "../pages/useState/UseState";
 
-
 export default function AppRoutes() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/useRef" element={<UseRef />}></Route>
-                <Route path="/useState" element={<UseState />}></Route>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/useRef" element={<UseRef />}></Route>
+        <Route path="/useState" element={<UseState />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
